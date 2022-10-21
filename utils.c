a6528ee5ff231fe5f5d20c63ee9e4959da2a7094
Azibhons <nathanazibabhonomeni@gmail.com>
2022-10-21 12:58:04 -0700

    Files update

--- utils.c
+++ utils.c
@@ -9,8 +9,7 @@
 int is_printable(char c)
 {
 	if (c >= 32 && c < 127)
-		return (1);
-
+	return (1);
 	return (0);
 }
 
@@ -26,7 +25,7 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 	char map_to[] = "0123456789ABCDEF";
 	/* The hexa format code is always 2 digits long */
 	if (ascii_code < 0)
-		ascii_code *= -1;
+	ascii_code *= -1;
 
 	buffer[i++] = '\\';
 	buffer[i++] = 'x';
@@ -46,7 +45,7 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 int is_digit(char c)
 {
 	if (c >= '0' && c <= '9')
-		return (1);
+	return (1);
 
 	return (0);
 }
@@ -61,9 +60,9 @@ int is_digit(char c)
 long int convert_size_number(long int num, int size)
 {
 	if (size == S_LONG)
-		return (num);
+	return (num);
 	else if (size == S_SHORT)
-		return ((short)num);
+	return ((short)num);
 
 	return ((int)num);
 }
@@ -78,9 +77,9 @@ long int convert_size_number(long int num, int size)
 long int convert_size_unsgnd(unsigned long int num, int size)
 {
 	if (size == S_LONG)
-		return (num);
+	return (num);
 	else if (size == S_SHORT)
-		return ((unsigned short)num);
+	return ((unsigned short)num);
 
 	return ((unsigned int)num);
 }
